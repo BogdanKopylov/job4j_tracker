a2db7c0db516427f5a1c23bf952e072c7818ea41
BogdanKopylov <bogdankopylov@gmail.com>
2020-11-11 21:17:55 +0200

    3. Интерфейс Input [#384660]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void addPassengers(int passengers) {
+
+    }
+
+    @Override
+    public double fill(double fuel) {
+        return 0;
+    }
+}
