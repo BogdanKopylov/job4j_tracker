2dafb01c363fd9932dccfa568609bcb331aaf124
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-07 22:23:49 +0200

    5. Подсчет функции в диапазоне. [#384455]

---
+++ src/test/java/ru/job4j/lambda/FunctionsTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FunctionsTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = Functions.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSquareFunction() {
+        List<Double> result = Functions.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExponentialFunction() {
+        List<Double> result = Functions.diapason(5, 8, x -> 2 * x);
+        List<Double> expected = Arrays.asList(10D, 12D, 14D);
+        assertThat(result, is(expected));
+    }
+}
