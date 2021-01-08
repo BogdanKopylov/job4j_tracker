6b312378afa5454f6dfab0cf83b9518d3c56fe02
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-08 20:03:20 +0200

    2. Список адресов [#384471]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    @Test
+    public void addressList() {
+        List<Profile> profiles = new ArrayList<>();
+        profiles.add(new Profile(new Address("Москва", "Профсоюзная", 4, 54)));
+        profiles.add(new Profile(new Address("Харьков", "Героев Труда", 5, 333)));
+        List<Address> expected = Arrays.asList(
+                new Address("Москва", "Профсоюзная", 4, 54),
+                new Address("Харьков", "Героев Труда", 5, 333));
+        assertThat(Profiles.collect(profiles), is(expected));
+    }
+
+}
