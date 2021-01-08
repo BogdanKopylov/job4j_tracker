bf2d19270da098b119f82fd7e581ea2cb421c001
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-08 15:29:14 +0200

    0. Stream API [#384469]

--- src/main/java/ru/job4j/stream/StreamAPI.java
+++ src/main/java/ru/job4j/stream/StreamAPI.java
@@ -8,8 +8,8 @@ import java.util.stream.Collectors;
 public class StreamAPI {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(0, 2, -5, 7, -33, 8, 901, -4));
-        List<Integer> positive = list.stream().
-                                filter(x -> x > 0).
-                                collect(Collectors.toList());
+        List<Integer> positive = list.stream()
+                                .filter(x -> x > 0)
+                                .collect(Collectors.toList());
     }
 }
