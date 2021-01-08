fb7fc72e845d3b14c226f0b54c32c7246a9ba9a8
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-08 20:20:31 +0200

    3. Уникальность элементов и сортировка. [#384472]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +8,11 @@ public class Profiles {
     static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(profile -> profile.getAddress())
+                .sorted(Comparator.comparing(Address::getCity)
+                        .thenComparing(Address::getStreet)
+                        .thenComparing(Address::getHome)
+                        .thenComparing(Address::getApartment))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -15,8 +15,14 @@ public class ProfilesTest {
         List<Profile> profiles = new ArrayList<>();
         profiles.add(new Profile(new Address("Москва", "Профсоюзная", 4, 54)));
         profiles.add(new Profile(new Address("Харьков", "Героев Труда", 5, 333)));
+        profiles.add(new Profile(new Address("Харьков", "Героев Труда", 5, 333)));
+        profiles.add(new Profile(new Address("Москва", "Профсоюзная", 3, 54)));
+        profiles.add(new Profile(new Address("Одесса", "Профсоюзная", 3, 54)));
+        profiles.add(new Profile(new Address("Одесса", "Профсоюзная", 3, 54)));
         List<Address> expected = Arrays.asList(
+                new Address("Москва", "Профсоюзная", 3, 54),
                 new Address("Москва", "Профсоюзная", 4, 54),
+                new Address("Одесса", "Профсоюзная", 3, 54),
                 new Address("Харьков", "Героев Труда", 5, 333));
         assertThat(Profiles.collect(profiles), is(expected));
     }
