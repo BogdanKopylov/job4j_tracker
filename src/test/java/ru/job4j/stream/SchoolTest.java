06015646ec421b9d12997a2a346bd29a6058e262
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-10 21:17:07 +0200

    0. Stream API улучшения [#384480]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -14,15 +14,15 @@ public class SchoolTest {
 
     @Before
     public void setUp() {
-        students.add(new Student(10, "Surname1"));
-        students.add(new Student(20, "Surname2"));
-        students.add(new Student(30, "Surname3"));
-        students.add(new Student(40, "Surname4"));
-        students.add(new Student(50, "Surname5"));
-        students.add(new Student(60, "Surname6"));
-        students.add(new Student(70, "Surname7"));
-        students.add(new Student(80, "Surname8"));
-        students.add(new Student(90, "Surname9"));
+        students.add(new Student("Surname1", 10));
+        students.add(new Student("Surname2", 20));
+        students.add(new Student("Surname3", 30));
+        students.add(new Student("Surname4", 40));
+        students.add(new Student("Surname5", 50));
+        students.add(new Student("Surname6", 60));
+        students.add(new Student("Surname7", 70));
+        students.add(new Student("Surname8", 80));
+        students.add(new Student("Surname9", 90));
     }
 
     @Test
@@ -31,9 +31,9 @@ public class SchoolTest {
         Predicate<Student> pr = student -> student.getScore() >= 70 && student.getScore() <= 100;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
-        expected.add(new Student(70, "Surname7"));
-        expected.add(new Student(80, "Surname8"));
-        expected.add(new Student(90, "Surname9"));
+        expected.add(new Student( "Surname7", 70));
+        expected.add(new Student("Surname8", 80));
+        expected.add(new Student("Surname9", 90));
         assertThat(rsl, is(expected));
     }
 
@@ -43,8 +43,8 @@ public class SchoolTest {
         Predicate<Student> pr = student -> student.getScore() >= 50 && student.getScore() < 70;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
-        expected.add(new Student(50, "Surname5"));
-        expected.add(new Student(60, "Surname6"));
+        expected.add(new Student("Surname5", 50));
+        expected.add(new Student("Surname6", 60));
         assertThat(rsl, is(expected));
     }
 
@@ -54,27 +54,27 @@ public class SchoolTest {
         Predicate<Student> pr = student -> student.getScore() > 0 && student.getScore() < 50;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
-        expected.add(new Student(10, "Surname1"));
-        expected.add(new Student(20, "Surname2"));
-        expected.add(new Student(30, "Surname3"));
-        expected.add(new Student(40, "Surname4"));
+        expected.add(new Student("Surname1", 10));
+        expected.add(new Student("Surname2", 20));
+        expected.add(new Student("Surname3", 30));
+        expected.add(new Student("Surname4", 40));
         assertThat(rsl, is(expected));
     }
 
     @Test
     public void collectToMap() {
         List<Student> students = Arrays.asList(
-                new Student(20, "Ivanov"),
-                new Student(10, "Zaicev"),
-                new Student(12, "Zaicev"),
-                new Student(13, "Stepanov"),
-                new Student(11, "Alekseev")
+                new Student("Ivanov", 20),
+                new Student("Zaicev", 10),
+                new Student("Zaicev", 12),
+                new Student("Stepanov", 13),
+                new Student("Alekseev", 11)
         );
         Map<String, Student> map = new HashMap<>();
-                map.put("Ivanov", new Student(20, "Ivanov"));
-                map.put("Zaicev", new Student(12, "Zaicev"));
-                map.put("Stepanov", new Student(13, "Stepanov"));
-                map.put("Alekseev", new Student(11, "Alekseev"));
+                map.put("Ivanov", new Student("Ivanov", 20));
+                map.put("Zaicev", new Student("Zaicev", 12));
+                map.put("Stepanov", new Student("Stepanov", 13));
+                map.put("Alekseev", new Student("Alekseev", 11));
         assertThat(School.collectToMap(students), is(map));
     }
 }
