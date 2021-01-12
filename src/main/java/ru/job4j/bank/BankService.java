2b916cf203a378fea8cdc6e8b7ed0683f79d4c69
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-12 19:16:52 +0200

    4. Optional в Stream API. [#384484]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -67,10 +67,9 @@ public class BankService {
      */
     public Optional<Account> findByRequisite(String passport, String requisite) {
         Optional<User> user = findByPassport(passport);
-        return user.map(value -> users.get(value).stream()
+        return user.flatMap(value -> users.get(value).stream()
                 .filter(account -> account.getRequisite().equals(requisite))
-                .findFirst())
-                .orElse(null);
+                .findFirst());
     }
 
     /**
