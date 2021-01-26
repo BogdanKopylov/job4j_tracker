66577b22558d900c352253b56646dbfc42e83d48
BogdanKopylov <bogdankopylov@gmail.com>
2021-01-26 21:29:28 +0200

    1. Экзамен. Базовый синтаксис. [#384593]1. Экзамен. Базовый синтаксис. [#384593]

--- src/main/java/ru/job4j/collection/Merge.java
+++ src/main/java/ru/job4j/collection/Merge.java
@@ -1,30 +1,26 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
+
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
         int leftIn = 0;
         int rightIn = 0;
         for (int i = 0; i < rsl.length; i++) {
-            while (leftIn < left.length || rightIn < right.length) {
                 if (rightIn == right.length) {
                     rsl[i] = left[leftIn];
                     leftIn++;
-                    break;
                 } else if (leftIn == left.length) {
                     rsl[i] = right[rightIn];
                     rightIn++;
-                    break;
                 } else if (left[leftIn] <= right[rightIn]) {
                     rsl[i] = left[leftIn];
                     leftIn++;
-                    break;
-                } else if (left[leftIn] > right[rightIn]) {
+                } else {
                     rsl[i] = right[rightIn];
                     rightIn++;
-                    break;
                 }
-            }
         }
         return rsl;
     }
