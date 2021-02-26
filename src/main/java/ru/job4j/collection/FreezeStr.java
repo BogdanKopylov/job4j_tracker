8100ec67d8b20b600194c2b3d28ee66fd3aa02df
BogdanKopylov <bogdankopylov@gmail.com>
2021-02-26 21:32:27 +0200

    Вторая задача - с помощью HashMap

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,7 +1,5 @@
 package ru.job4j.collection;
 
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,13 +7,16 @@ public class FreezeStr {
     public static boolean eq(String left, String right) {
         Map<Character, Integer> map = new HashMap<>();
         for(char ch : left.toCharArray()) {
-            map.putIfAbsent(ch, 0);
             map.computeIfPresent(ch, (a, b) -> b + 1);
+            map.putIfAbsent(ch, 1);
         }
         for(char ch : right.toCharArray()) {
-            map.putIfAbsent(ch, 0);
             map.computeIfPresent(ch, (a, b) -> b - 1);
+            map.putIfAbsent(ch, 1);
+            if (map.get(ch) == 0) {
+                map.remove(ch);
+            }
         }
-        return map.values().stream().allMatch(i -> i == 0);
+        return map.isEmpty();
     }
 }
